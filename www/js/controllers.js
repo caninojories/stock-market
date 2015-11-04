0ee903112533d65dbf75d5640a5246bce2788591
Jo-Ries Canino <canino_jories@hotmail.com>
2015-11-04 15:00:31 +0800

    first commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,16 +41,147 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+.controller('MyStocks', ['$scope',
+  function($scope) {
+    $scope.myStocksArray = [
+      {ticker: 'AAPL '},
+      {ticker: 'GPRO'},
+      {ticker: 'FB'},
+      {ticker: 'NFLX'},
+      {ticker: 'TLSA'},
+      {ticker: 'BRK-A'},
+      {ticker: 'INTC'},
+      {ticker: 'MSFT'},
+      {ticker: 'GE'},
+      {ticker: 'BAC'},
+      {ticker: 'C'},
+      {ticker: 'T'},
+    ];
+  }])
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('Stock', ['$scope', '$stateParams', '$window', 'stockDataService', 'dateService', 'chartDataService',
+  function($scope, $stateParams, $window, stockDataService, dateService, chartDataService) {
+    $scope.ticker     = $stateParams.stockTicker;
+    $scope.chartView  = 4;
+
+    $scope.$on('$ionicView.afterEnter', function() {
+      getPriceData();
+      getDetailsData();
+      getChartData();
+    });
+
+    $scope.chartViewFunction = function(n) {
+      $scope.chartView = n;
+    };
+
+    function getDetailsData() {
+      var promise = stockDataService.getDetailsData($scope.ticker);
+
+      promise.then(function(data) {
+        $scope.stockDetailsData = data;
+      });
+    }
+
+    function getPriceData() {
+      var promise = stockDataService.getPriceData($scope.ticker);
+
+      promise.then(function(data) {
+        $scope.stockPriceData = data;
+        console.log(data);
+        if (data.chg_percent >= 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#33cd5f'};
+        } else if (data.chg_percent < 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#ef473a'};
+        }
+      });
+    }
+
+    $scope.oneYearAgoDate = dateService.oneYearAgoDate();
+    $scope.todayDate      = dateService.currentDate();
+
+    function getChartData() {
+      var promise = chartDataService.historicalData($scope.ticker, $scope.oneYearAgoDate, $scope.todayDate);
+
+      promise.then(function(data) {
+        $scope.data = data.map(function(series) {
+          series.values = series.values.map(function(d) {
+            return {
+              x: d[0], y: d[1]
+            };
+          });
+
+          return series;
+        });
+      });
+    }
+
+    var marginBottom = ($window.innerWidth / 100) * 5;
+
+    $scope.options = {
+      chart: {
+        type: 'linePlusBarChart',
+        height: 500,
+        tooltips: false,
+        showLegend: false,
+        useVoronoi: false,
+        xShowMaxMin: false,
+        useInteractiveGuideline:false,
+        yShowMaxMin: false,
+        margin: {
+            top: 30,
+            right: 75,
+            bottom: marginBottom,
+            left: 75
+        },
+        bars: {
+            forceY: [0]
+        },
+        bars2: {
+            forceY: [0]
+        },
+        color: ['#2ca02c', 'darkred'],
+        x: function(d, i) {return i;},
+        xAxis: {
+          axisLabel: '',
+          tickFormat: function(d) {
+            var dx = $scope.data[0].values[d] && $scope.data[0].values[d].x || 0;
+            if (dx > 0) {
+              return d3.time.format('%b %d')(new Date(dx));
+            }
+
+            return null;
+          }
+        },
+        x2Axis: {
+          tickFormat: function(d) {
+            var dx = $scope.data[0].values[d] && $scope.data[0].values[d].x || 0;
+            return d3.time.format('%b %Y')(new Date(dx));
+          },
+          showMaxMin: false
+        },
+        y1Axis: {
+          axisLabel: 'Volume',
+          tickFormat: function(d){
+              return d3.format(',f')(d);
+          }
+        },
+        y2Axis: {
+          axisLabel: 'Price',
+          tickFormat: function(d) {
+              return '$' + d3.format('s')(d);
+          }
+        },
+        y3Axis: {
+          axisLabel: 'Volume',
+          tickFormat: function(d){
+            return d3.format(',.2s')(d);
+          }
+        },
+        y4Axis: {
+          tickFormat: function(d) {
+            return '$' + d3.format(',.2s')(d);
+          }
+        }
+      }
+    };
+  }]);
